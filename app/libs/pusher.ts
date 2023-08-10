4bb1836ce5140048dc1a2dd5950ea4469df4f59a
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-08-10 15:38:54 +0200

    Real time messages, conversations, read receipts and active status with Pusher

---
+++ app/libs/pusher.ts
@@ -0,0 +1,14 @@
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+export const pusherServer = new PusherServer({
+  appId: process.env.app_id!,
+  key: process.env.key!,
+  secret: process.env.secret!,
+  cluster: "eu",
+  useTLS: true,
+});
+
+export const pusherClient = new PusherClient(process.env.key!, {
+  cluster: "eu",
+});
